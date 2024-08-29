d368e61228d69ab9ed62fdf16c1abfb97e135473
cmhinst2 <cmhinst2@gmail.com>
2024-08-29 17:38:42 +0900

    240829 연산자

---
+++ 02_operator/src/edu/kh/op/practice/OperatorPractice3.java
@@ -0,0 +1,47 @@
+package edu.kh.op.practice;
+
+import java.util.Scanner;
+
+public class OperatorPractice3 {
+	
+	public void practice2() {
+/*
+
+ * ATM에서 사용자가 원하는 금액을 입력하면, 
+ * 5만원, 1만원, 5천원, 1천원 단위로 최소 개수의 지폐로 돈을 인출하라 
+ * ex) 사용자가 123000원을 입력하면, 5만원 지폐 2장, 
+ * 1만원 지폐 2장, 5천원 지폐 0장, 1천원 지폐 3장이 필요함
+ * 
+ */
+		Scanner sc = new Scanner(System.in);
+ 
+		System.out.print("출금할 금액 입력 : ");
+		int amount = sc.nextInt();
+		
+		int fiftyThousand = amount / 50000;
+		amount %= 50000;
+		
+		int tenThousand = amount / 10000;
+		amount %= 10000;
+		
+		int fiveThousand = amount / 5000;
+		amount %= 5000;
+		
+		int oneThousand = amount / 1000;
+		
+		System.out.println("50000원 : " + fiftyThousand);
+		System.out.println("10000원 : " + tenThousand);
+		System.out.println("5000원 : " + fiveThousand);
+		System.out.println("1000원 : " + oneThousand);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
--- 02_operator/src/edu/kh/op/practice/run/PracticeRun.java
+++ 02_operator/src/edu/kh/op/practice/run/PracticeRun.java
@@ -1,6 +1,7 @@
 package edu.kh.op.practice.run;
 
 import edu.kh.op.practice.OperatorPractice;
+import edu.kh.op.practice.OperatorPractice3;
 
 public class PracticeRun { // 기능 실행용 클래스
 
@@ -9,7 +10,11 @@ public class PracticeRun { // 기능 실행용 클래스
 		
 		OperatorPractice op = new OperatorPractice();
 		
-		op.practice1();
+		//op.practice1();
+		
+		OperatorPractice3 op3 = new OperatorPractice3();
+		
+		op3.practice2();
 
 	}
 
