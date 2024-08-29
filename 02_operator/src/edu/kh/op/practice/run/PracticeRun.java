20f5e0ba072dd2a01131e13fc9e0f1864bd40831
cmhinst2 <cmhinst2@gmail.com>
2024-08-29 17:12:55 +0900

    apply .gitignore

---
+++ 02_operator/src/edu/kh/op/practice/run/PracticeRun.java
@@ -0,0 +1,16 @@
+package edu.kh.op.practice.run;
+
+import edu.kh.op.practice.OperatorPractice;
+
+public class PracticeRun { // 기능 실행용 클래스
+
+	// 메인메서드 필수
+	public static void main(String[] args) {
+		
+		OperatorPractice op = new OperatorPractice();
+		
+		op.practice1();
+
+	}
+
+}
