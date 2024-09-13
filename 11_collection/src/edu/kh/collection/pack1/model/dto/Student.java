d402a9532bfdffc4b050a760273865b066a2a881
cmhinst2 <cmhinst2@gmail.com>
2024-09-13 15:32:58 +0900

    List 삭제,검색,정렬까지

--- 11_collection/src/edu/kh/collection/pack1/model/dto/Student.java
+++ 11_collection/src/edu/kh/collection/pack1/model/dto/Student.java
@@ -1,6 +1,6 @@
 package edu.kh.collection.pack1.model.dto;
 
-public class Student{
+public class Student implements Comparable<Student>{
 	// 필드
 	private String name; // 이름
 	private int age; //나이
@@ -65,6 +65,20 @@ public class Student{
 	public String toString() {
 		return "Student [name=" + name + ", age=" + age + ", region=" + region + ", gender=" + gender + ", score="
 				+ score + "]";
+	}
+
+	@Override
+	public int compareTo(Student other) {
+		// compareTo() : 객체를 정렬하기 위해 사용하는 메서드
+		// 두 객체를 비교하고 그 결과로 정수를 반환함
+		// 현재 객체의 값과 다른 객체의 값을 비교함
+		
+		// 현재객체의 나이 - 다른객체의 나이 -> 비교함
+		// 값이 양수면 현재 객체가 더 크다
+		// 음수면 더 작다 
+		
+		//return this.age - other.age; // 나이를 기준으로 오름차순 정렬
+		return other.age - this.age; // 나이를 기준으로 내림차순 정렬
 	}
 	
 
