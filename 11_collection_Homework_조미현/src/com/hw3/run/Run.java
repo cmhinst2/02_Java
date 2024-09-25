0045337c58d20227d3a4af34656838e714ba80bd
cmhinst2 <cmhinst2@gmail.com>
2024-09-25 16:13:10 +0900

    컬렉션문제 도서,장난감공장

--- 11_collection_Homework_조미현/src/com/hw3/run/Run.java
+++ 11_collection_Homework_조미현/src/com/hw3/run/Run.java
@@ -1,9 +1,13 @@
 package com.hw3.run;
 
+import com.hw3.model.service.BookService;
+
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		BookService service = new BookService();
+		service.displayMenu();
 
 	}
 
--- 11_collection_Homework_조미현/src/com/toyFactory/run/Run.java
+++ 11_collection_Homework_조미현/src/com/toyFactory/run/Run.java
@@ -1,9 +1,13 @@
 package com.toyFactory.run;
 
+import com.toyFactory.model.service.ToyFactory;
+
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ToyFactory factory = new ToyFactory();
+		factory.displayMenu();
 
 	}
 
